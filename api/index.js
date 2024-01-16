e3624c25dbb3011e45f2e12802121aecfc48140d
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-17 01:07:43 +0700

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,5 +16,6 @@ app.listen(3001,()=>{
     console.log('Server đang chạy ở port 3001!')
 })
 
+app.use("/api/user",userRouter)
 
 
