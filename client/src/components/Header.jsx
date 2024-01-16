9565a9693c1889248f06a7a63563ac25198017cf
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-17 00:15:07 +0700

    custom navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-gradient-to-r from-slate-200 via-slate-300 to-slate-400 shadow-md">
         <div className='flex justify-around items-center max-w-6xl mx-auto p-3'>
             <Link to="/">
                 <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
@@ -18,20 +18,43 @@ const Header = () => {
             <FaSearch className='text-slate-500'/>
         </form>
 
-        <ul className='flex gap-8'>
+        <ul className='flex gap-8 font-semibold'>
             {/* Sử dụng hidden sm:inline để khi thu nhỏ screen thì không xuất hiện */}
-            <Link to="/">
-                <li className="hidden sm:inline text-slate-700 hover:underline hover:text-white">Home</li>
+            <Link 
+                to="/"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="group relative inline-block cursor-pointer hover:text-brightColor">
+                <li className="hidden sm:inline text-slate-700 hover:text-white">
+                    Home
+                    {/* Hiệu ứng trượt underline */}
+                    <span className="absolute inset-x-0 bottom-0 h-0.5 bg-white transform scale-x-0 origin-left transition-transform group-hover:scale-x-100"></span>
+                    </li>
             </Link>
             
-            <Link to="/about">
-                <li className="hidden sm:inline text-slate-700 hover:underline hover:text-white">About</li>
+            <Link 
+                to="/about"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="group relative inline-block cursor-pointer hover:text-brightColor">
+                <li className="hidden sm:inline text-slate-700  hover:text-white">
+                    About
+                    <span className="absolute inset-x-0 bottom-0 h-0.5 bg-white transform scale-x-0 origin-left transition-transform group-hover:scale-x-100"></span>
+                    </li>
             </Link>
            
-           <Link to="/sign-in">
-                <li className=" text-slate-700 hover:underline hover:text-white">
+           <Link 
+                to="/sign-in"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="group relative inline-block cursor-pointer hover:text-brightColor">
+                <li className=" text-slate-700 hover:text-white">
                 {''}
                 Sign in
+                <span className="absolute inset-x-0 bottom-0 h-0.5 bg-white transform scale-x-0 origin-left transition-transform group-hover:scale-x-100"></span>
                 </li>
            </Link>
             
