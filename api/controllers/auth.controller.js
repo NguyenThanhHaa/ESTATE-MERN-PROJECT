224f3a440b169583450e4910177dad27871dc39c
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-17 01:56:10 +0700

    validate password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,8 +2,19 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
 
+// Tối thiểu tám ký tự, ít nhất một chữ cái, một số và một ký tự đặc biệt
+const validatePasword = (password) => {
+    return password.match(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+    );
+  };
+
 export const signup = async (req,res)=>{
     const {username,email,password} = req.body
+
+    if (!validatePasword(req.body.password)) {
+        return res.status(400).json("Mật khẩu không hợp lệ!")
+    }
 
     //Băm password
     const hashedPassword = bcryptjs.hashSync(password,10)
