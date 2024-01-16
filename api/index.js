603dc3488d24019848c58c2f3d1419a503574425
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-17 00:45:39 +0700

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connect MongoDB thành công!")
+}).catch((err)=>{
+    console.log(err)
+})
 
 const app = express();
 
 app.listen(3001,()=>{
     console.log('Server đang chạy ở port 3001!')
 })
+
+
 
