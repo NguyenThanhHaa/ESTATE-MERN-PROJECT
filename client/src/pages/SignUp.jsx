2b1307e7de579b49b51ef16964a1246be6a1e59e
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-19 12:41:55 +0700

    create show/hide password in sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { IoEyeSharp } from "react-icons/io5";
+import { IoEyeOffSharp } from "react-icons/io5";
+
 
 const SignUp = () => {
+  const [password, setPassword] = useState('');
+
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isConfirmPassword, setIsConfirmPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
+  const handleToggleConfirmPassword = () =>{
+    setIsConfirmPassword(!isConfirmPassword)
+  }
+
+  const handleOnChangePassword= (value) => {
+    setPassword(value)
+  }
+
   return (
     //Canh object ở giữa thì sử dụng mx-auto
     <div className='p-3 max-w-lg mx-auto'>
@@ -19,18 +39,47 @@ const SignUp = () => {
           className='border p-3 rounded-lg' 
           id='email'/>
 
-        <input 
-          type="password" 
-          placeholder='Password' 
-          className='border p-3 rounded-lg' 
-          id='password'/>
+        <div className='flex  relative  items-center'>
+          <input  
+                type={isShowPassword ? 'text' : 'password'}
+                onClick={() => setIsShowPassword(!isShowPassword)}
+                placeholder='Password' 
+                className='border p-3 rounded-lg w-full' 
+                id='password'/>
 
-        <input 
-          type="password" 
-          placeholder='Confirm password' 
-          className='border p-3 rounded-lg' 
-          id='confirm-password'/>
+          <button
+            type="button"
+            onClick={handleTogglePassword}
+            className="text-xl absolute inset-y-0 right-5"
+            aria-label={isShowPassword ? 'Hide password' : 'Show password'}
+          >
+            {isShowPassword ? <IoEyeSharp /> : <IoEyeOffSharp />}
+          </button>
+           
+        </div>
 
+        <div className='flex  relative  items-center'>
+          <input 
+            type={isConfirmPassword ? 'text' : 'password'}
+            onClick={() => setIsConfirmPassword(!isConfirmPassword)}
+            onChange={handleOnChangePassword}
+            placeholder='Confirm password' 
+            className='border p-3 rounded-lg w-full' 
+            id='confirm-password'/>
+
+          <button
+            type="button"
+            onClick={handleToggleConfirmPassword}
+            onChange={handleOnChangePassword}
+            className="text-xl absolute inset-y-0 right-5"
+            aria-label={isConfirmPassword ? 'Hide password' : 'Show password'}
+          >
+            {isConfirmPassword ? <IoEyeSharp /> : <IoEyeOffSharp />}
+          </button>
+
+        </div>
+        
+       
         <button className="bg-slate-700 text-white rounded-lg py-2 uppercase hover:bg-slate-500 disabled:opacity-80">Sign Up</button>
 
       </form>
