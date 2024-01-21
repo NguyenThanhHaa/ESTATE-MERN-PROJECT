0888e9b76d86e740b42291a313da93093c064443
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-21 15:40:12 +0700

    handle errors and validate fields in sign in and sign up controllers

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,42 +4,38 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
 
-// Tối thiểu tám ký tự, ít nhất một chữ cái, một số và một ký tự đặc biệt
-const validatePasword = (password) => {
-    return password.match(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
-    );
-  };
-
 export const signup = async (req,res,next)=>{
     const {username,email,password} = req.body
 
-    // if (!validatePasword(req.body.password)) {
-    //     return res.status(400).json("Mật khẩu không hợp lệ!")
-    // }
+    //Kiểm tra user tồn tại trong database
+    const validEmail = await User.findOne({email});
+    const validUserName = await User.findOne({username});
 
-    //Băm password
-    const hashedPassword = bcryptjs.hashSync(password,10)
+    if(validEmail){
+        return next(errorHandler(400,'Tài khoản đã tồn tại!!!!'));
+    }
 
-    //Lưu xuống database 
+    if(validUserName){
+        return next(errorHandler(400,'Tài khoản đã tồn tại!!!!'));
+    }
+
+    const validPassword = password.match(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+
+    if(!validPassword){
+        return next(errorHandler(404,'Mật khẩu cần tối thiểu 8 ký tự, ít nhất một chữ cái, một số và một ký tự đặc biệt'));
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password,10);
+
     const newUser = new User({username,email,password:hashedPassword})
-
-    //Bắt lỗi 
-    try {
-        // Save user to database
+    try{
         await newUser.save();
-        res.status(201).json("Tạo tài khoản thành công!");
-    } catch (error) {
-        // // Handle specific errors
-        // if (error.code === 11000) { // Duplicate key (MongoDB)
-        //     res.status(409).json("Tài khoản đã tồn tại!!!");
-        // } else {
-        //     // Handle other errors
-        //     res.status(500).json("Đã có lỗi xảy ra khi tạo tài khoản!");
-        // }
-
+        res.status(201).json('Tạo tài khoản thành công!');
+    }catch(error){
         next(error);
     }
+
 };
 
 export const signin = async(req,res,next) =>{
@@ -55,7 +51,7 @@ export const signin = async(req,res,next) =>{
 
         //Vì password đã được hashed dưới database, nên để kiểm tra tính valid của password.
         //Cần sử dụng method .compareSync( ) của bcryptjs
-        // method .compareSync( ): check password đã được hashed 
+        // method .compareSync( ): so sánh password với password đã được hashed 
         const validPassword = bcryptjs.compareSync(password,validUser.password);
 
         if(!validPassword){
