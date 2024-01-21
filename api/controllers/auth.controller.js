fc17f7fce650911289d22def0569c6fb8b4877ab
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-21 13:04:15 +0700

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
 // Tối thiểu tám ký tự, ít nhất một chữ cái, một số và một ký tự đặc biệt
 const validatePasword = (password) => {
@@ -9,12 +10,12 @@ const validatePasword = (password) => {
     );
   };
 
-export const signup = async (req,res)=>{
+export const signup = async (req,res,next)=>{
     const {username,email,password} = req.body
 
-    if (!validatePasword(req.body.password)) {
-        return res.status(400).json("Mật khẩu không hợp lệ!")
-    }
+    // if (!validatePasword(req.body.password)) {
+    //     return res.status(400).json("Mật khẩu không hợp lệ!")
+    // }
 
     //Băm password
     const hashedPassword = bcryptjs.hashSync(password,10)
@@ -23,12 +24,20 @@ export const signup = async (req,res)=>{
     const newUser = new User({username,email,password:hashedPassword})
 
     //Bắt lỗi 
-    try{
-        await newUser.save()
-        res.status(201).json("Tạo tài khoản thành công!")
-    }catch(error){
-        res.status(500).json("Tài khoản đã tồn tại!")
-    }
-   
+    try {
+        // Save user to database
+        await newUser.save();
+        res.status(201).json("Tạo tài khoản thành công!");
+    } catch (error) {
+        // // Handle specific errors
+        // if (error.code === 11000) { // Duplicate key (MongoDB)
+        //     res.status(409).json("Tài khoản đã tồn tại!!!");
+        // } else {
+        //     // Handle other errors
+        //     res.status(500).json("Đã có lỗi xảy ra khi tạo tài khoản!");
+        // }
 
-}
+        next(error);
+    }
+};
+   
