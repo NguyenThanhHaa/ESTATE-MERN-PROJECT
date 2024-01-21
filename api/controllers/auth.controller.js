729e7f67f9c085f0b2157109232f9a4a9fd13897
thanh ha <thanhhanguyen6103@gmail.com>
2024-01-21 13:54:03 +0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 
-import User from "../models/user.model.js"
-import bcryptjs from 'bcryptjs'
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 // Tối thiểu tám ký tự, ít nhất một chữ cái, một số và một ký tự đặc biệt
 const validatePasword = (password) => {
@@ -40,4 +41,38 @@ export const signup = async (req,res,next)=>{
         next(error);
     }
 };
+
+export const signin = async(req,res,next) =>{
+    const {email,password} = req.body;
+
+    try{
+        //Kiểm tra user tồn tại trong database
+        const validUser = await User.findOne({email});
+
+        if(!validUser){
+            return next(errorHandler(404,'Không tìm thấy người dùng'));
+        }
+
+        //Vì password đã được hashed dưới database, nên để kiểm tra tính valid của password.
+        //Cần sử dụng method .compareSync( ) của bcryptjs
+        // method .compareSync( ): check password đã được hashed 
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+
+        if(!validPassword){
+            return next(errorHandler(404,'Mật khẩu không hợp lệ!'));
+        }
+
+        const token = jwt.sign({id:validUser._id,}, process.env.JWT_SECRET)
+
+        // password: pass - Destruturing : lấyy giá trị password gán vào biến pass
+        // ...rest : lấy tất cả các thuộc tính còn lại và gán vào biến rest
+        const {password:pass,...rest} = validUser._doc;
+
+        //Save the token as the cookie
+        //httpOnly:true có nghĩa là other thỉd party applications can't have access to our cookie
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest); // chỉ trả về rest - các thuộc tính còn lại, mà không trả password của user về => bảo mật tốt hơn. 
+    }catch(error){
+        next(error);
+    }
+}
    
