6c668dc814a1e2eeef962ad0b3078e173857f21e
thanh ha <thanhhanguyen6103@gmail.com>
2024-05-04 23:41:29 +0700

    complete upload listing image functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,7 +1,88 @@
-import { Checkbox } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import {getStorage, uploadBytesResumable, ref, getDownloadURL} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js'
+import { app } from '../firebase';
+
+
 
 const CreateListing = () => {
+    const [files,setFiles] = useState([])
+    const [uploading, setUploading] = useState(false);
+    
+
+
+    const [formData,setFormData] = useState({
+        imageUrls: [],
+
+    })
+
+    const [imageUploadError,setImageUploadError] = useState(false);
+    // console.log(formData)
+
+    const handleImageSubmit = (e) =>{
+        if(files.length > 0 && files.length + formData.imageUrls.length <7){
+            setUploading(true);
+            setImageUploadError(false);
+            const promises = [];
+
+            for(let i =0; i<files.length; i++){
+                promises.push(storeImage(files[i]));
+            }
+            Promise.all(promises).then((urls)=>{
+                setFormData({...formData, imageUrls:formData.imageUrls.concat(urls)});
+
+                setImageUploadError(false);
+                setUploading(false);
+         
+            }).catch((err)=>{
+                setImageUploadError('Tải ảnh thất bại! (chỉ cho phép kích thước mỗi ảnh tối đa: 2mb)');
+                setUploading(false);
+            })
+            
+        }
+        else if(files.length<=0){
+            setImageUploadError('Vui lòng tải ít nhất 1 ảnh!');
+            setUploading(false);
+        }
+        else{
+            setImageUploadError('Vui lòng chỉ tải tối đa 6 ảnh cho một lần tạo danh sách!');
+            setUploading(false);
+        }
+
+    }
+
+    const storeImage = async(file) =>{
+        return new Promise((resolve,reject)=>{
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage,fileName);
+            const uploadTask =uploadBytesResumable(storageRef,file);
+            uploadTask.on(
+                "state_changed",
+                (snapshot)=>{
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Đang tải ${progress}%`)
+                },
+                (error)=>{
+                    reject(error);
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                        resolve(downloadURL);
+                    })
+                }
+            )
+
+        }) 
+    }
+
+    const handleRemoveImage = (index) => {
+        setFormData({
+            ...formData,
+            imageUrls:formData.imageUrls.filter((_,i)=> i !== index)
+        })
+    }
+
+   
   return (
     <main className="p-3 max-w-4xl mx-auto">
     <h1 className="text-3xl text-center font-semibold my-7">TẠO DANH SÁCH</h1> 
@@ -112,20 +193,59 @@ const CreateListing = () => {
                 <span className="font-normal text-gray-600 "> Ảnh đầu tiên được chọn sẽ là ảnh bìa (tối đa: 6 ảnh)</span>
             </p>
             <div className="my-5 flex gap-5 items-center " >
-                <input type="file" id="images" accept="image/*"
+                <input
+                onChange={(e)=>setFiles(e.target.files)} 
+                type="file" 
+                id="images" 
+                accept="image/*"
                 multiple
                 className=" border border-dashed border-gray-500 p-3 rounded-md w-full
                 bg-slate-300"/>
-                <button className="p-3 text-white
+                <button
+                type='button'
+                disabled={uploading} 
+                onClick={handleImageSubmit}
+                className="py-4 px-10 text-white
                 bg-slate-700 borrder borrder-green-700 rounded-md uppercase hover:bg-slate-500 dissabled:opacity-80
                 font-semibold
-                 ">Tải</button>
+                 ">{uploading ? "😴" : 'Tải'}</button>
             </div>
+            
+            {imageUploadError &&(
+                <p className="bg-red-100 rounded-md text-red-700 text-center py-2 px-2 mb-5">{imageUploadError}</p>
+            )}
+
+            {
+                formData.imageUrls.length>0 && formData.imageUrls.map((url,index)=>
+                    (
+                    <div
+                    key={url} 
+                    className="flex justify-between p-3 border rounded-md items-center  my-2 bg-slate-50">
+                        <img 
+                        src={url} 
+                        alt="Hình ảnh liên quan" 
+                        className="w-32 h-32 my-5 object-cover rounded-md"/>
+
+                        <button
+                        type="button"
+                        onClick={()=>handleRemoveImage(index)}
+                        className="text-red-700 p-3 font-semibold hover:opacity-75">
+                            Xóa
+                        </button>
+                    </div>
+                        
+                )
+                )
+            }
+
             <button 
             className="p-3 text-white
             bg-cyan-900 borrder borrder-green-700 rounded-md uppercase hover:bg-slate-800 dissabled:opacity-80
-            font-semibold">Tạo danh sách</button>
+            font-semibold mt-3">Tạo danh sách</button>
+
+            
         </div>
+       
     </form> 
 
         
