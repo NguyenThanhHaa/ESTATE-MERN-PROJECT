74a5cee5fa1a308a43b0fd1d57dfd72e00f3e817
thanh ha <thanhhanguyen6103@gmail.com>
2024-05-30 13:08:18 +0700

    complete show user listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -47,6 +47,12 @@ const Profile = () => {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  const [showListingsError, setShowListingsError] = useState(false);
+
+  const [userListings, setUserListings] = useState([]);
+
+  const [isShowListings,setIsShowListings] = useState(false);
+
   
   const handleTogglePassword = () => {
     setIsShowPassword(!isShowPassword);
@@ -177,6 +183,37 @@ const handleSignOut = async () => {
     dispatch(deleteUserFailure(error.message));
   }
 };
+
+  const handleShowListings = async ()=>{
+    try{
+
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`,{
+        method:'GET'
+      });
+
+      const data = await res.json();
+
+      if(data.success === false){
+        setShowListingsError(true);
+        return; 
+      }
+      setUserListings(data);
+      console.log(data);
+    }catch(error){
+      setShowListingsError(true);
+      setIsShowListings(false);
+    }
+  }
+
+  // Check listings were showed or not
+  const handleIsOpenListings = async() => {
+    setIsShowListings(true);
+    await handleShowListings();
+   
+  };
+  const handleIsCloseListings = () => setIsShowListings(false);
+
   return (
     <div className='p-5 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold mb-7'>Hồ sơ người dùng</h1>
@@ -262,7 +299,6 @@ const handleSignOut = async () => {
           </Link>
         </button>
         
-        
         <div className="flex justify-between">
         <div className="text-red-600 hover:cursor-pointer font-semibold hover:text-red-800"
           onClick={handleOpen}
@@ -279,11 +315,53 @@ const handleSignOut = async () => {
       {updateSuccess ? (
         <div className='bg-green-200 p-3 rounded-md font-semibold text-center text-green-800 uppercase'>Cập nhật thành công!</div>
       ) : ''}
+
+      <div className=" w-full text-center text-green-800 rounded-md uppercase hover:text-green-700 font-semibold "
+              >
+                {isShowListings ? 
+                  <p 
+                    onClick={handleIsCloseListings}
+                    className="cursor-pointer"
+                    >Đóng danh sách</p> 
+                  : <p 
+                      onClick={handleIsOpenListings}
+                      className="cursor-pointer"
+                      >Xem danh sách đã tạo</p>}
+        </div>
+
+        <p>{showListingsError ? 'Có lỗi trong quá trình hiển thị. Vui lòng thử lại sau!' : ''}</p>
+      
+      {
+        isShowListings ? (
+            userListings && userListings.length >0 && (userListings.map((listing)=>(
+            <div key={listing._id} className="bg-white border rounded-lg p-3 flex justify-between items-center gap-5">
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} className="h-16 w-16 rounded-md"/>
+              
+              </Link>
+
+              <Link to={`/listing/${listing._id}`}>
+                {/* truncate: when text is too long, it will show ... */}
+                <div className="font-semibold hover:underline truncate">{listing.name}</div>
+              </Link>
+
+              <div className="flex gap-5">
+                <div className="text-green-700 font-semibold cursor-pointer">Sửa</div>
+                <div className="text-gray-300">|</div>
+                <div className="text-red-700 font-semibold cursor-pointer">Xóa</div>
+              </div>
+              
+            </div>
+        )))) : ''
+      } 
    
       </form>
+
+      
       
 
       <Modal
+        className="modal-delete-user"
         open={isOpen}
         onClose={handleClose}
       >
@@ -311,6 +389,36 @@ const handleSignOut = async () => {
 
       </Modal>
 
+
+      {/* <Modal
+        className="modal-show-listings"
+        open={isShowListings}
+        onClose={handleIsCloseListings}
+      >
+       <Box className="bg-slate-100 top-1/2 bottom-1/2 mx-auto mt-72 w-2/6 rounded-xl border-none">
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-end px-2 py-1">
+                <IoIosClose className=" text-black font-bold cursor-pointer size-7" onClick={handleClose} />
+              </div>
+              
+              <div className="mb-3">
+                <h1 className="text-center font-bold my-4 uppercase text-red-800">Bạn có chắc chắn xóa tài khoản?</h1>
+              </div>
+              
+              <div className="flex justify-center gap-5 my-5 mx-5 px-2">
+                <button className="bg-slate-800 text-white rounded-md px-2 py-2 hover:bg-slate-600"
+                  onClick={handleDeleteUser}
+                >Đồng ý</button>
+                <button className="bg-gray-400 text-slate-900 rounded-md px-4 py-2 hover:bg-slate-500"
+                  onClick={handleIsCloseListings}
+                >Hủy</button>
+              </div>
+            </div>
+
+        </Box>
+        
+      </Modal>
+       */}
 
       
     </div>
