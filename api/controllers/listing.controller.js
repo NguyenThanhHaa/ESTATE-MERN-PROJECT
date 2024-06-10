937015c94ee73b1897d0ff2283f36f4cbf8f0a3e
thanh ha <thanhhanguyen6103@gmail.com>
2024-06-10 20:58:41 +0700

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -30,3 +30,29 @@ export const deleteListing = async(req,res,next)=>{
          next(error);
       }
 }
+
+export const updateListing = async (req,res,next) => {
+   const listing = await Listing.findById(req.params.id);
+
+   if(!listing){
+      return next(errorHandler(404, 'Không tìm thấy danh sách'))
+   }
+
+   if(req.user.id !== listing.userRef){
+      return next(errorHandler(401, 'Chỉ người dùng mới có thể tự cập nhật danh sách của chính mình!'));
+   }
+
+   try{
+      const updatedListing = await Listing.findByIdAndUpdate(
+         req.params.id,
+         req.body,
+         {
+            new: true
+         }
+      )
+
+      res.status(200).json(updatedListing)
+   }catch(error){
+      next(error);
+   }
+}
