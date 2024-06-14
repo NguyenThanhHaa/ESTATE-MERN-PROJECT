1fe6ef8c9d7822ffb9968e336f73e2b57cf500f7
thanh ha <thanhhanguyen6103@gmail.com>
2024-06-14 16:26:50 +0700

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const About = () => {
+export default function About() {
   return (
-    <div>
-      about
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-10 '>Giới thiệu <span className="text-slate-600">Hee<span className="text-slate-800">Estate</span></span></h1>
+      <p className='mb-10 text-slate-700 text-justify'>Hee Estate là trang web mua nhà đáng tin cậy, được thiết kế để mang lại trải nghiệm mua sắm bất động sản dễ dàng và thuận tiện nhất cho khách hàng. Với sứ mệnh giúp mọi người tìm thấy ngôi nhà mơ ước của mình, Hee Estate cam kết cung cấp các thông tin minh bạch, chính xác và đa dạng về các loại hình bất động sản, từ căn hộ chung cư, nhà phố cho đến biệt thự cao cấp.</p>
+      <p className='mb-10 text-slate-700 text-justify'>
+      Đội ngũ chuyên gia của Hee Estate luôn sẵn sàng hỗ trợ bạn trong mọi giai đoạn của quá trình mua nhà, từ việc tìm kiếm, lựa chọn cho đến hoàn tất giao dịch. Chúng tôi hiểu rằng việc mua nhà là một quyết định quan trọng và đầy thách thức, do đó, Hee Estate không chỉ cung cấp các công cụ tìm kiếm thông minh mà còn tư vấn tận tình, giúp bạn đưa ra quyết định đúng đắn.
+      </p>
+      <p className='mb-10 text-slate-700 text-justify'>Hee Estate tự hào về kho dữ liệu bất động sản phong phú, được cập nhật liên tục để đảm bảo khách hàng luôn tiếp cận những thông tin mới nhất và chính xác nhất. Mỗi ngôi nhà được giới thiệu trên trang web đều trải qua quy trình kiểm duyệt nghiêm ngặt để đảm bảo chất lượng và sự phù hợp với nhu cầu của khách hàng. Chúng tôi cung cấp hình ảnh, mô tả chi tiết và các thông tin cần thiết về tiện ích, vị trí, giá cả, giúp bạn dễ dàng so sánh và lựa chọn.</p>
     </div>
   )
 }
-
-export default About
