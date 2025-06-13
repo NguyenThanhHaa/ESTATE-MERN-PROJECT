c4da3676c4cab3ce4d4e0f6020148456fda9ca56
thanh ha <thanhhanguyen6103@gmail.com>
2025-06-13 17:52:41 +0700

    hotfix

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "login-cc393.firebaseapp.com",
-  projectId: "login-cc393",
-  storageBucket: "login-cc393.appspot.com",
-  messagingSenderId: "748233380782",
-  appId: "1:748233380782:web:8f1f9540b101c4f8bfae3f"
+  apiKey: "AIzaSyDBsTXHrnGzYHO6OgAz2u4-Z1lYN6L_ZnE",
+  authDomain: "mern-stack-d85dd.firebaseapp.com",
+  projectId: "mern-stack-d85dd",
+  storageBucket: "mern-stack-d85dd.firebasestorage.app",
+  messagingSenderId: "669886699454",
+  appId: "1:669886699454:web:f5e5e1c9c79c3542580bef"
 };
 
 
